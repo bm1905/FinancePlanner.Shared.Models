6736f80bf5e9b6018295e76006754e3238b5c373
bm1905 <bijay.maharjan5@gmail.com>
2022-11-25 12:00:05 -0600

    Added new exception type

---
+++ FinancePlanner.Shared.Models/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace FinancePlanner.Shared.Models.Exceptions
+{
+    [Serializable]
+    public class UnauthorizedException : ApplicationException
+    {
+        public UnauthorizedException() { }
+
+        public UnauthorizedException(Type type) : base($"{type} is missing") { }
+
+        public UnauthorizedException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+        public UnauthorizedException(string message) : base(message) { }
+
+        public UnauthorizedException(string message, Exception innerException) : base(message, innerException) { }
+
+        public UnauthorizedException(string message, string stackTrace) : base(message)
+        {
+            StackTrace = stackTrace;
+        }
+
+        public override string? StackTrace { get; }
+    }
+}
