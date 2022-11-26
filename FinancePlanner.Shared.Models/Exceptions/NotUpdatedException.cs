627e4b1a9dcd5ac07de1a59eaac362c587f6cfee
bm1905 <bijay.maharjan5@gmail.com>
2022-11-26 13:30:55 -0600

    Added NotUpdatedException exception

---
+++ FinancePlanner.Shared.Models/Exceptions/NotUpdatedException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace FinancePlanner.Shared.Models.Exceptions;
+
+[Serializable]
+public class NotUpdatedException : ApplicationException
+{
+    public NotUpdatedException() { }
+
+    public NotUpdatedException(Type type) : base($"{type} is missing") { }
+
+    public NotUpdatedException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+    public NotUpdatedException(string message) : base(message) { }
+
+    public NotUpdatedException(string message, Exception innerException) : base(message, innerException) { }
+
+    public NotUpdatedException(string message, string stackTrace) : base(message)
+    {
+        StackTrace = stackTrace;
+    }
+
+    public override string? StackTrace { get; }
+}
