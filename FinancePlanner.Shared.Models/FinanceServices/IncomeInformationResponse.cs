cb5876f178e336c9e60d2771fa307d6edba18d32
bm1905 <bijay.maharjan5@gmail.com>
2022-11-26 16:19:40 -0600

    Add IncomeInformationRequest and Response

---
+++ FinancePlanner.Shared.Models/FinanceServices/IncomeInformationResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace FinancePlanner.Shared.Models.FinanceServices;
+
+public class IncomeInformationResponse
+{
+    public int IncomeInformationId { get; set; }
+    public string UserId { get; set; } = string.Empty;
+    public string EmployeeName { get; set; } = string.Empty;
+    public int PayInformationId { get; set; }
+    public decimal GrossPay { get; set; }
+    public decimal NetPay { get; set; }
+    public decimal TotalHours { get; set; }
+    public decimal PayRate { get; set; }
+    public decimal TotalPreTaxDeductions { get; set; }
+    public decimal TotalPostTaxDeductions { get; set; }
+    public decimal StateAndFederalTaxableWages { get; set; }
+    public decimal SocialAndMedicareTaxableWages { get; set; }
+}
