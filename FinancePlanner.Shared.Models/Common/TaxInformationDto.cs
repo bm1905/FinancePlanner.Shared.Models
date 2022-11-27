fc499af4913dc7eaf7ff19005deee2d5b7731561
bm1905 <bijay.maharjan5@gmail.com>
2022-11-26 22:10:03 -0600

    Make properties nullable type

--- FinancePlanner.Shared.Models/Common/TaxInformationDto.cs
+++ FinancePlanner.Shared.Models/Common/TaxInformationDto.cs
@@ -7,14 +7,19 @@ namespace FinancePlanner.Shared.Models.Common;
 /// </summary>
 public class TaxInformationDto
 {
+    // Common
     public W4Type W4Type { get; set; }
     public TaxFilingStatus TaxFilingStatus { get; set; }
     public int PayPeriodNumber { get; set; }
     public string State { get; set; } = string.Empty;
-    public bool IsMultipleJobsChecked { get; set; }
     public decimal ExtraWithholdingAmount { get; set; }
-    public decimal DeductionsAmount { get; set; }
-    public decimal OtherIncomeAmount { get; set; }
-    public decimal ClaimDependentsAmount { get; set; }
-    public int AllowanceNumber { get; set; }
+
+    // W4 Before 2020
+    public int? AllowanceNumber { get; set; }
+
+    // W4 From or After 2020
+    public bool? IsMultipleJobsChecked { get; set; }
+    public decimal? DeductionsAmount { get; set; }
+    public decimal? OtherIncomeAmount { get; set; }
+    public decimal? ClaimDependentsAmount { get; set; }
 }
