8dd7f441af8544b6c93b56c29063f396ad21956b
bm1905 <bijay.maharjan5@gmail.com>
2022-11-29 16:33:25 -0600

    Remove IncomeInformationDto

--- FinancePlanner.Shared.Models/FinanceServices/IncomeInformationRequest.cs
+++ FinancePlanner.Shared.Models/FinanceServices/IncomeInformationRequest.cs
@@ -4,5 +4,15 @@ namespace FinancePlanner.Shared.Models.FinanceServices;
 
 public class IncomeInformationRequest
 {
-    public IncomeInformationDto IncomeInformation { get; set; } = new();
+    public string UserId { get; set; } = string.Empty;
+    public string EmployeeName { get; set; } = string.Empty;
+    public int PayInformationId { get; set; }
+    public decimal GrossPay { get; set; }
+    public decimal NetPay { get; set; }
+    public decimal TotalHours { get; set; }
+    public decimal PayRate { get; set; }
+    public decimal TotalPreTaxDeductions { get; set; }
+    public decimal TotalPostTaxDeductions { get; set; }
+    public TaxableWageInformationDto TaxableWageInformation { get; set; } = new();
+    public TaxWithheldInformationDto TaxWithheldInformation { get; set; } = new();
 }
