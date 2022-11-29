80486887ed8e7b567eee116eecc25449556ae6c3
bm1905 <bijay.maharjan5@gmail.com>
2022-11-29 16:14:40 -0600

    Refactor IncomeInformationResponse

--- FinancePlanner.Shared.Models/FinanceServices/IncomeInformationResponse.cs
+++ FinancePlanner.Shared.Models/FinanceServices/IncomeInformationResponse.cs
@@ -5,8 +5,5 @@ namespace FinancePlanner.Shared.Models.FinanceServices;
 public class IncomeInformationResponse
 {
     public int IncomeInformationId { get; set; }
-    public string UserId { get; set; } = string.Empty;
-    public string EmployeeName { get; set; } = string.Empty;
-    public int PayInformationId { get; set; }
     public IncomeInformationDto IncomeInformation { get; set; } = new();
 }
